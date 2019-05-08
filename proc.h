778970607e6cc211423eaab32ebe7bcb299b5a96
MrFlynn <nick@pleatsikas.me>
2019-05-08 15:28:55 -0700

    Adding priority to process struct.

--- proc.h
+++ proc.h
@@ -50,6 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int status;                  // Process exit status.
+  int priority;                // Process priority. Used by scheduler.
 };
 
 // Process memory is laid out contiguously, low addresses first:
