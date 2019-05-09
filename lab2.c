f6a07780164c60cbb4200a11ad6e9af0bf387cbe
MrFlynn <nick@pleatsikas.me>
2019-05-09 15:57:10 -0700

    Added tests for priority aging.

--- lab2.c
+++ lab2.c
@@ -2,12 +2,12 @@
 #include "user.h"
 
 int main(int argc, char *argv[]) {
-	int PScheduler(void);
+  int PScheduler(void);
 
   printf(1, "\n This program tests the correctness of your lab#2\n");
   
-	PScheduler();
-	return 0;
+  PScheduler();
+  return 0;
 }
   
     
@@ -23,29 +23,29 @@ int PScheduler(void) {
   printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
   setpriority(0);
   for (i = 0; i <  3; i++) {
-	  pid = fork();
-	  if (pid > 0 ) {
-		  continue;
+    pid = fork();
+    if (pid > 0 ) {
+      continue;
     } else if ( pid == 0) {
-  		setpriority(30-10*i);	
-		  for (j=0;j<50000;j++) {
-			  for(k=0;k<1000;k++) {
-				  asm("nop"); 
+      setpriority(30-10*i);  
+      for (j=0;j<50000;j++) {
+        for(k=0;k<1000;k++) {
+          asm("nop"); 
         }
       }
-		  printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
-		  exit(0);
+      printf(1, "\n child# %d with original priority %d has finished with priority %d! \n",getpid(),30-10*i,getpriority());    
+      exit(0);
     } else {
-			printf(2," \n Error \n");
-		}
-	}
+      printf(2," \n Error \n");
+    }
+  }
 
-	if(pid > 0) {
-		for (i = 0; i <  3; i++) {
-			wait(0);
-		}
+  if(pid > 0) {
+    for (i = 0; i <  3; i++) {
+      wait(0);
+    }
       printf(1,"\n if processes with highest priority finished first then its correct \n");
   }
-	exit(0);		
-	return 0;
+  exit(0);    
+  return 0;
 }
